d21e1cb0946741813a3625a522e0af4fb040c7dd
dangthanhan <88368817+DangThanhAn@users.noreply.github.com>
2022-11-21 14:23:18 +0700

    feature: TextField

--- src/app/textfield/textfield.component.ts
+++ src/app/textfield/textfield.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-textfield',
@@ -8,8 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class TextfieldComponent implements OnInit {
 
   constructor() { }
+  @Input() widthExp : string | undefined;
+  @Input() contentPlacehoder : string | any;
 
+  // @Input() width : string | undefined;
   ngOnInit(): void {
+
   }
 
 }
